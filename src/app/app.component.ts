b5345930766bd6c692e6791534853d7cceec1989
superbromy <florian.iob@gmail.com>
2024-04-20 00:14:36 +0200

    add form to transform players to tdf players

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PlayersFormComponent } from './players-form/players-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PlayersFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
