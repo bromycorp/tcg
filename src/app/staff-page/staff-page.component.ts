2e3848f0e9928b9193d723eba6383b0507e78fe9
Florian IOB <florian.iob@gmail.com>
2024-05-15 01:35:37 +0200

    moving folders intos pages folders

---
+++ src/app/staff-page/staff-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-staff-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './staff-page.component.html',
+  styleUrl: './staff-page.component.scss'
+})
+export class StaffPageComponent {
+
+}
