94d468bc238ece05a4f59a7ef0bcec7b4189eecd
Florian IOB <florian.iob@gmail.com>
2024-12-20 21:27:24 +0100

    shaping file download for pairings

--- src/app/player-page/player-pairings/player-pairings.component.ts
+++ src/app/player-page/player-pairings/player-pairings.component.ts
@@ -4,45 +4,60 @@ import {
   HttpHeaders,
 } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { AngularFireStorage, AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { AngularFireDatabase, AngularFireDatabaseModule, AngularFireList } from '@angular/fire/compat/database';
+import {
+  AngularFireStorage,
+  AngularFireStorageModule,
+} from '@angular/fire/compat/storage';
+import {
+  AngularFireDatabase,
+  AngularFireDatabaseModule,
+  AngularFireList,
+} from '@angular/fire/compat/database';
 import { FileUpload } from '../../models/fileupload';
+import { FileUploadService } from '../../services/file-upload.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-player-pairings',
   standalone: true,
-  imports: [HttpClientModule, AngularFireDatabaseModule, AngularFireStorageModule],
+  imports: [
+    HttpClientModule,
+    AngularFireDatabaseModule,
+    AngularFireStorageModule,
+  ],
   templateUrl: './player-pairings.component.html',
   styleUrl: './player-pairings.component.scss',
 })
 export class PlayerPairingsComponent implements OnInit {
-  pairingsUrl: string =
-    'https://firebasestorage.googleapis.com/v0/b/bromycorp.appspot.com/o/test.PNG?alt=media&token=6dba9145-2e96-404e-a5d4-2b67c9da6977';
-  private basePath = '/uploads';
+  fileUploads?: any[];
 
-  constructor(private http: HttpClient,
-    private fireStorage: AngularFireStorage,
-    private database: AngularFireDatabase
+  constructor(
+    private uploadService: FileUploadService
   ) {}
 
   ngOnInit() {
-    console.log(this.getFiles());
     const headers = new HttpHeaders();
-    headers.append('Access-Control-Allow-Origin', 'http://localhost:4200');
-    this.http
-      .get(
-        'https://bromycorp-default-rtdb.europe-west1.firebasedatabase.app/',
-        {
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-          },
-        }
+
+    this.uploadService
+      .getFiles(6)
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          // store the key
+          changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))
+        )
       )
-      .subscribe((_) => console.log(_));
+      .subscribe((fileUploads) => {
+        fileUploads.forEach(file => (
+          console.log("file" +file.file)
+        ))
+        this.fileUploads = fileUploads;
+        console.log(fileUploads[0].file)
+      });
+
+    //headers.append('Access-Control-Allow-Origin', 'http://localhost:4200');
+    //this.http.get('https://bromycorp-default-rtdb.europe-west1.firebasedatabase.app/',{headers: {'Access-Control-Allow-Origin': '*',},}).subscribe((_) => console.log(_));
   }
 
-  getFiles(): AngularFireList<FileUpload> {
-    return this.database.list(this.basePath, ref =>
-      ref.limitToLast(1));
-  }
+  
 }
